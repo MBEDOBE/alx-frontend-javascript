16ceeee26fdc76824c9ea60f601b9a7b2952e7cd
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-06-26 19:19:25 +0000

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const employees = reportWithIterator.allEmployees;
-  const departmentKeys = Object.keys(employees);
-  let employeeNames = '';
-
-  for (let key of departmentKeys) {
-    const departmentEmployees = employees[key];
-    employeeNames += departmentEmployees.join(' | ') + ' ';
-  }
-
-  return employeeNames.trim();
+  return reportWithIterator.join(" | ");
 }
