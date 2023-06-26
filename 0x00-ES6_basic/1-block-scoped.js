b0347d5e351a2079848088451b5b3704e5f91aa4
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-06-26 18:04:28 +0000

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -5,7 +5,7 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     let task = true;
     let task2 = false;
-    if ((task && task2)) {
+    if (task && task2) {
       return [task, task2];
     }
   }
