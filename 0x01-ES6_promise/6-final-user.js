2fd35928004baae951b7ce3b3cc734d1e97e3b58
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-06-27 16:20:15 +0000

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ];
-
-  return Promise.allSettled(promises).map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason.message
-  }));
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  const result = [];
+  return Promise.allSettled(promises).then((results) => {
+    results.map(({ status, value, reason }) =>
+      result.push({
+        status,
+        value: status === 'rejected' ? reason.toString() : value,
+      })
+    );
+    return result;
+  });
 }
