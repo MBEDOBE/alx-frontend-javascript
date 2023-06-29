c3cbb2f9a66c2ad64129ad3adfe94f087e5f36f7
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-06-29 23:11:46 +0000

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,13 @@
 export default class Building {
   constructor(sqft) {
+    if (
+      this.constructor !== Building &&
+      typeof this.evacuationWarningMesagge !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
+    }
     this._sqft = sqft;
   }
 
@@ -7,9 +15,7 @@ export default class Building {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
-    throw new Error(
-      'Class extending Building must override evacuationWarningMessage',
-    );
+  set sqft(sqft) {
+    this._sqft = sqft;
   }
 }
